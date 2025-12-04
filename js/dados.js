8698541de66cb7cd3b15bf43cbc69642135cca36
victorbispo <dlsejogosdaoras@gmail.com>
2025-12-04 10:06:21 -0300

    4/12

---
+++ js/dados.js
@@ -0,0 +1,15 @@
+const livros = [
+  { id: 1, img: "images/capas/codigolimpo.jpg", nome: "Código Limpo" },
+  { id: 2, img: "images/capas/domcasmurro.jpg", nome: "Dom Casmurro" },
+  { id: 3, img: "images/capas/filhasdalua.jpg", nome: "Filhas da Lua" },
+  { id: 4, img: "images/capas/tiparanegocios.jpg", nome: "TI para Negócios" },
+  { id: 5, img: "images/capas/mestresdotempo.jpg", nome: "Mestres do Tempo" },
+  { id: 6, img: "images/harryordem.jpg", nome: "Harry Potter e as Relíquias da Morte" }
+];
+
+const categorias = [
+  { id: 1, img: "images/categorias/fantasia.png", nome: "Fantasia" },
+  { id: 2, img: "images/categorias/ficcao.png", nome: "Ficção" },
+  { id: 3, img: "images/categorias/romance.png", nome: "Romance" },
+  { id: 4, img: "images/categorias/tecnologia.png", nome: "Tecnologia" },
+  { id: 5, img: "images/categorias/aventura.png", nome: "Aventura" },];
